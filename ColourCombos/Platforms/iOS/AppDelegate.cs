696d4ba27cf0a59d4e3bf4872845ed683b3308bc
dan-giddins <daniel.giddins112@gmail.com>
2023-07-12 18:54:58 +0100

    Add project files.

---
+++ ColourCombos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ColourCombos;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
